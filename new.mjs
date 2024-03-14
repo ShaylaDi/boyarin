02cdfd5bca7bb682245611b6f50fc68c3b43e141
ShaylaBimbo <160532062+ShaylaDi@users.noreply.github.com>
2024-03-14 17:29:35 +0700

    Update new.mjs

--- new.mjs
+++ new.mjs
@@ -3,6 +3,8 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next')Config1} */
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig = {};
 
 export default withNextIntl(nextConfig); sukarskih invalid
